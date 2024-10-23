26206820515d7de7a42ddfc332619bd11f64be36
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-10-23 14:35:23 +0200

    fix bug reservation

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
     }
 
     fetchClasses();
-  }, [selectedDateForFetch]);
+  }, [reload, selectedDateForFetch]);
 
   useEffect(() => {
     async function fetchProfiles() {
