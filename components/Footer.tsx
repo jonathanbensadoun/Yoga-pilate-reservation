d7be9afe9fd9235d58d23b919c130d77a27d039f
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-10-27 16:48:36 +0100

    change layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 
 export default function Footer() {
   const nameURL = process.env.NEXT_PUBLIC_NAME_URL;
-
   return (
-    <footer className=" bg-gray-50 flex flex-col md:flex-row justify-center items-center p-4 gap-2">
+    <footer className="mt-20 bg-gray-50 flex flex-col md:flex-row justify-center items-center p-4 gap-2">
       <p>2024 Laure Sautier - Tous droits réservés ©</p>
       <div className="flex flex-row gap-2">
         <Link href={`${nameURL}/auth/terms`}>Conditions Générales</Link>
