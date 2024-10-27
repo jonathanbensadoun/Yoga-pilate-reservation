64ae4a3e2e0f0a431615b5fd6cf6dd07eb4505ba
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-10-27 17:50:05 +0100

    change link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,13 @@
 import Link from "next/link";
 
 export default function Footer() {
-  const nameURL = process.env.NEXT_PUBLIC_NAME_URL;
   return (
     <footer className="mt-20 bg-gray-50 flex flex-col md:flex-row justify-center items-center p-4 gap-2">
       <p>2024 Laure Sautier - Tous droits réservés ©</p>
       <div className="flex flex-row gap-2">
-        <Link href={`${nameURL}/auth/terms`}>Conditions Générales</Link>
-        <Link href={`${nameURL}/auth/privacy`}>Confidentialité</Link>
-        <Link href={`${nameURL}/auth/faq`}>FAQ</Link>
+        <Link href={`/auth/terms`}>Conditions Générales</Link>
+        <Link href={`/auth/privacy`}>Confidentialité</Link>
+        <Link href={`/auth/faq`}>FAQ</Link>
       </div>
     </footer>
   );
